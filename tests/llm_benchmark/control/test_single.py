fe1257e2206f22d250adab933b92c9d41e4496ba
Matthew <matthew.truscott@turintech.ai>
2023-12-12 17:16:55 +0000

    test: update singleloop to match cpp test

--- tests/llm_benchmark/control/test_single.py
+++ tests/llm_benchmark/control/test_single.py
@@ -5,14 +5,14 @@ import pytest
 from llm_benchmark.control.single import SingleForLoop
 
 
-@pytest.mark.parametrize("n, S", [(0, 0), (1, 0), (2, 1), (10, 45), (100, 4950)])
+@pytest.mark.parametrize("n, S", [(0, 0), (1, 0), (2, 1), (3, 3), (4, 6), (10, 45)])
 def test_sum_range(n: int, S: int) -> None:
     assert SingleForLoop.sum_range(n) == S
 
 
 @pytest.mark.parametrize(
     "v, M",
-    [([0], 0), ([1, 2, 3], 3), ([3, 2, 1], 3), ([3, 3, -1], 3), ([-1, -1, -3], -1)],
+    [([0], 0), ([1, 2, 3, 4, 5], 5), ([1, 1, 1, 1, 0], 1), ([-1, -1, -1, -1, -1], 0)],
 )
 def test_max_list(v: List[int], M: int) -> None:
     assert SingleForLoop.max_list(v) == M
@@ -21,14 +21,14 @@ def test_max_list(v: List[int], M: int) -> None:
 @pytest.mark.parametrize(
     "n, m, S",
     [
-        (1, 1, 0),
-        (10, 1, 45),
+        (0, 2, 0),
+        (1, 2, 0),
+        (2, 2, 0),
+        (3, 2, 2),
+        (4, 2, 2),
         (10, 2, 20),
         (10, 3, 18),
-        (10, 12, 0),
-        (100, 3, 1683),
-        (100, 12, 432),
-        (101, 100, 100),
+        (10, 4, 12),
     ],
 )
 def test_sum_modulus(n: int, m: int, S: int) -> None:
